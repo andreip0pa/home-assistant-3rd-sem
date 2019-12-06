fc97acfc71071cdadd4b6587459e453523fc7db5
Andrei Popa <andreip0pa44@gmail.com>
2019-12-06 12:56:04 +0100

    Create Conversation.cs

---
+++ Listener/UDPListener/UDPListener/Conversation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UDPListener
+{
+    public class Conversation
+    {
+
+        // Variables
+        #region
+        public string question;
+        public string answer;
+        public int id;
+        public DateTime timeOfConversation;
+        #endregion
+
+        // Properties
+        #region
+        public string Question
+        {
+            get { return question; }
+            set { question = value; }
+        }
+        public string Answer
+        {
+            get { return answer; }
+            set { answer = value; }
+        }
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public DateTime TimeOfConversation
+        {
+            get { return timeOfConversation; }
+            set { timeOfConversation = value; }
+        }
+        #endregion
+
+        // Constructors
+        #region
+        public Conversation(int _id, string _question, string _answer,  DateTime _timeOfConversation)
+        {
+            ID = _id;
+            Question = _question;
+            Answer = _answer;            
+            TimeOfConversation = _timeOfConversation;
+        }
+
+        // Empty constructor for JSON
+        public Conversation()
+        {
+
+        }
+        #endregion
+    }
+}
