c992b5dbf09785fcf59948c45cddc1612b7e42b9
WillBlackney <44806865+WillBlackney@users.noreply.github.com>
2019-12-07 13:04:07 +0100

    Rest Service Added

---
+++ Conversation Rest Service/Conversation Rest Service/Solutions/VS Solutions/Conversation Rest Service/Startup.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+
+
+namespace ConversationRESTService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Create a new 'CORS' policy called 'CorsPolicy', and set it to allow all origins, headers and methods
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+
+            // Set up SWAGGER
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Conversations API", Version = "v1.0" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+             c.SwaggerEndpoint("/swagger/v1/swagger.json", "Conversations API v1.0")
+            );
+
+            // Use the CORS policy we created earlier
+            app.UseCors("CorsPolicy");
+            app.UseMvc();
+        }
+    }
+}
---
+++ Conversation Rest Service/Conversation Rest Service/Solutions/VS Solutions/Conversations Setup Query.sql
@@ -0,0 +1,13 @@
+﻿Create Table Conversations
+(
+ID int primary Key NOT NULL,
+Question varchar (5000),
+Answer varchar (5000),
+TimeOfConversation varchar(100)
+);
+
+GO
+Insert into Conversations Values (1,'What is the weather like today?',
+'Today it is 12 degrees with a chance of rain', '5:17:01 6/12/2019');
+Insert into Conversations Values (2,'Tell me a joke',
+'Your life', '7:22:38 7/12/2019');
